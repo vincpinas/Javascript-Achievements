55ff61efabda3a213e005874bbd8b7cdb8117276
vincent <ikbengerrit92@gmail.com>
2020-11-19 13:26:41 +0100

    :package: REPOSITORY MERGE

---
+++ Les 1/tijd.js
@@ -0,0 +1,6 @@
+let mybutton = document.getElementById("MyButton");
+let date = new Date();
+
+mybutton.addEventListener('click', ()=>{
+    window.alert(date + "\nIk ben Vincent Pinas\nIk woon in Uithoorn\nEn ik zit op het Media College Amsterdam");
+})
