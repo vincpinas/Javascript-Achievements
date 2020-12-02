43b468b0cf59b00e475dc994178dd6e81535b482
vincent <ikbengerrit92@gmail.com>
2020-12-02 14:17:33 +0100

    :package: MEET THE DOM ACHIEVEMENT DONE

--- Les 1/tijd.js
+++
@@ -1,6 +0,0 @@
-let mybutton = document.getElementById("MyButton");
-let date = new Date();
-
-mybutton.addEventListener('click', ()=>{
-    window.alert(date + "\nIk ben Vincent Pinas\nIk woon in Uithoorn\nEn ik zit op het Media College Amsterdam");
-})
