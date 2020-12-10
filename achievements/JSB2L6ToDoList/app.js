923870851e437943e3583ab267f6eae711a1f94d
vincpinas <70635677+vincpinas@users.noreply.github.com>
2020-12-10 22:08:19 +0100

    :package: LES 6 TO DO LIST PARTLY DONE

---
+++ achievements/JSB2L6ToDoList/app.js
@@ -0,0 +1,80 @@
+let AddToDo = document.getElementById('AddToDo');
+let FinToDo = document.getElementById('FinishToDo');
+let RemToDo = document.getElementById('RemoveToDo');
+let inputField = document.getElementById('inputField');
+let toDoContainer = document.getElementById('toDoContainer');
+const popUp = document.querySelector('.popUpAlert');
+const toDoItems = [];
+
+// Adding a To Do.
+AddToDo.addEventListener('click', () => {
+
+    if (inputField.value.length > 0) {
+        toDoItems.push(inputField.value)
+        updateList()
+    }
+
+    if (inputField.value.length == 0) {
+        Alert()
+    } else {
+        toDoContainer.classList.add("to-dos--active")
+    }
+
+    inputField.value = "";
+});
+
+
+// Removing a To Do.
+RemToDo.addEventListener('click', () => {
+    if (isNaN(inputField.value) == false) {
+        try {
+            deleteItem()
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    if (inputField.value.length == 0) {
+        Alert()
+    }
+});
+
+
+// Finishing a To Do.
+FinToDo.addEventListener('click', () => {
+    FinishItem()
+});
+
+
+
+// Functions
+function updateList() {
+    toDoContainer.innerHTML = ""
+    toDoItems.forEach(function(n) {
+        toDoContainer.innerHTML += "<li>"+n+"</li>"
+    })
+}
+
+function deleteItem() {
+    let ParsedInt = parseInt(inputField.value)
+    let index = ParsedInt - 1
+    toDoItems.splice(index, 1);
+    updateList()
+}
+
+function FinishItem() {
+    let ParsedInt = parseInt(inputField.value)
+    let index = ParsedInt - 1
+    toDoItems[index].style.color = 'red'
+    updateList()
+}
+
+async function Alert() { 
+    popUp.classList.add("popUpAlert--active")
+    await sleep(4400)
+    popUp.classList.remove("popUpAlert--active")
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
